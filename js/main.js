343294cbe8d0af2e69a74f133ebfa1f08e0345f7
Alexander Varnaev <alexandervarnaev@gmail.com>
2018-08-14 22:32:01 +0300

    added fullscrmenu

---
+++ js/main.js
@@ -0,0 +1,19 @@
+console.log ('я подключен!');
+// const hamburger = document.querySelector (".hamburger-menu");
+// const fullscreenmenu = document.getElementsByClassName ('fullscreen-menu');
+// const closeMenu = document.querySelector (".fullscreen-menu__close");
+
+// hamburger.addEventListener ('click', function (e){
+//     fullscreenmenu.style.display = "flex";
+// });
+// closeMenu = addEventListener ('click', function (e){
+//     e.preventDefault ();
+//     fullscreenmenu.style.display = "none";
+// })
+// // var b = document.createElement('div');
+// // b.classList.add('123');
+// // document.body.appendChild(b);
+
+// var fullscrmenu = document.getElementsByClassName ('nav__list')[0];
+// fullscrmenu.classList.add("fromjs");
+
